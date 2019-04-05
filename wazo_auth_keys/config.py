0e606a048495e5c7fe1223e55e908348bf6fff12
Francois Blackburn <fblackburn@wazo.community>
2019-04-05 09:51:13 -0400

    allow to define internal user in conf.d
    used when you need to add extra stevedore plugin with other acl

--- wazo_auth_keys/config.py
+++ wazo_auth_keys/config.py
@@ -1,5 +1,7 @@
-# Copyright 2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+
+import os
 
 from xivo.chain_map import ChainMap
 from xivo.config_helper import parse_config_dir, read_config_file_hierarchy
@@ -7,6 +9,9 @@ from xivo.config_helper import parse_config_dir, read_config_file_hierarchy
 from wazo_auth_cli.config import (
     _DEFAULT_CONFIG,
 )
+
+SERVICES_CONFIG_FILE = 'config.yml'
+SERVICES_EXTRA_CONFIG = 'conf.d'
 
 
 def _read_user_config(parsed_args):
@@ -21,3 +26,15 @@ def build(parsed_args):
     system_file_config = read_config_file_hierarchy(ChainMap(user_file_config, _DEFAULT_CONFIG))
     final_config = ChainMap(user_file_config, system_file_config, _DEFAULT_CONFIG)
     return final_config
+
+
+def load_services(parsed_args):
+    services_dir = parsed_args.config
+    services_config = {
+        'config_file': os.path.join(services_dir, SERVICES_CONFIG_FILE),
+        'extra_config_files': os.path.join(services_dir, SERVICES_EXTRA_CONFIG),
+    }
+    services = read_config_file_hierarchy(services_config)
+    services.pop('config_file', None)
+    services.pop('extra_config_files', None)
+    return services
